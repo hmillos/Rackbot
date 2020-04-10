9a05f5d787e8e0f45790578b40dd9ec2a3d7b7c6
hmillos <hmillos13@gmail.com>
2020-04-10 18:52:48 -0500

    Service request page

--- rackbot-app2/src/App.js
+++ rackbot-app2/src/App.js
@@ -1,55 +1,79 @@
 import React from "react";
-import { MemoryRouter, Switch, Route } from "react-router-dom";
-
+import { MemoryRouter} from "react-router-dom";
+import Form from 'react-bootstrap/Form';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import ButtonToolbar from "react-bootstrap/ButtonToolbar";
-import { LinkContainer } from "react-router-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 import "./App.css";
 
-const Home = () => <span>Home</span>;
 
-const About = () => <span>About</span>;
-
-const Users = () => <span>Users</span>;
-
+function Barra(){
+  return(<Navbar bg="primary" variant="dark" expand="lg">
+  <Navbar.Brand href="/home">Rackbot</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="home">Home</Nav.Link>
+      <Nav.Link href="link">Iniciar Sesión</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>);
+}
+function Copyright() {
+  return (
+    <div>
+    {'Copyright © '}
+      <a href="http://www.rackbot.co">Rackbot</a>
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </div>
+  );
+}
 const App = () => (
   <MemoryRouter>
-    <Container className="p-3" >
+    <Container className="p-3" maxWidth="xs" >
+      <Barra />
       <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <h2>
-          "Current Page is {" "}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </h2>
-        <h2>
-          Navigate to{" "}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
-        </h2>
+        <h2>Solicitar Servicio</h2>
+        <Form>
+          <Form.Group controlId="formDescripcion">
+            <Form.Label>Descripción del problema</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+            <Form.Text className="text-muted">
+              Describe el problema con el mayor detalle
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formAddress">
+            <Form.Label>Tu dirección</Form.Label>
+            <Form.Control type="none" placeholder="" />
+            <Form.Text className="text-muted">
+              Nuestro técnico te visitará en esta dirección
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control type="email" placeholder="Ingresa tu coreo Electrónico" />
+          </Form.Group>
+          <Form.Group controlId="formPhone">
+            <Form.Label>Número de contacto</Form.Label>
+            <Form.Control type="number" placeholder="" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
       </Jumbotron>
     </Container>
+
+
+
+    <Copyright />
   </MemoryRouter>
 );
 
